4132a8656eb885164ecc7fe613684a1aa059de36
Alexandra Hanley <alexandrahanley@gmail.com>
2015-04-06 11:44:59 -0600

    update collection select to omit user selection

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -6,15 +6,17 @@ class AppointmentsController < ApplicationController
     @users = User.all
     @user = User.new
     @appointment = Appointment.new
+    @appointments = Appointment.all
   end
 
   def create
-    @doctor = Doctor.find(params[:doctor_id])
-    @user = User.find(params[:user_id])
-
     @appointment = Appointment.new(appointment_params)
-    @appointment.doctor_id = @doctor.id
-    @appointment.user_id = @user.id
+
+    # @doctor = Doctor.find(params[:doctor_id])
+    # @user = User.find(params[:user_id])
+    #
+    # @appointment.doctor_id = @doctor.id
+    # @appointment.user_id = @user.id
       if @appointment.save
       redirect_to appointments_path
       else
